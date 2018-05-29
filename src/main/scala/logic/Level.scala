8a121269089acc48b409e302a1cdb80ae2fb9b9b
lychanl <lycha_@poczta.onet.pl>
2018-05-29 19:42:52 +0200

    Documentation for remaining classes

--- src/main/scala/logic/Level.scala
+++ src/main/scala/logic/Level.scala
@@ -1,9 +1,21 @@
 package logic
 
+/**
+  * Enumeration of game difficulty levels
+  * 
+  * @note calculations for MEDIUM and HARD may take some time, minutes or more for game beginning 
+  */
 object Level extends Enumeration {
   type Level = Value
   val EASY, MEDIUM, HARD = Value
   
+  /**
+    * Gets AI prediction analysis depth for given difficulty level
+    *  
+    * @param value Difficulty level
+    * 
+    * @return depth for given difficulty level
+    */
   def getLevelDepth(value: Value) : Int = {
     value match {
       case EASY => 2
--- src/main/scala/logic/package.scala
+++ src/main/scala/logic/package.scala
@@ -1,5 +1,8 @@
 import logic.Level.Value
 
+/**
+  * Enumeration of players
+  */
 package object PlayerName extends Enumeration {
   type PlayerName = Value
   val COMPUTER, PLAYER = Value
